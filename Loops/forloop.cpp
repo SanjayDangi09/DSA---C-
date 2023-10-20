369e386e742fb7cc382b0c1c5e78e904a846bd84
sanjaydangi09 <sanjaydangi9358@gmail.com>
2023-10-20 21:45:13 +0530

    for loop added

---
+++ Loops/forloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std ;
+
+int main () {
+
+    int num ;
+
+    cout << " Enter the stop value : ";
+    cin >> num ;
+
+    for( int i =0 ; i< num ; i++)
+    {
+        cout << i << " " ;
+
+    }
+}
